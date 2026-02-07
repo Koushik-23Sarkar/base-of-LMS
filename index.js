f430743984d45b4ccdb294f2d80614776a46539c
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2026-02-07 21:09:22 +0530

    Protecting application with sql xss and other attacks

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 import morgan from "morgan";
 import express from "express";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+// import xss from "xss-clean";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+// Global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // limit each IP to 100 requests per window (here per 15 minutes)
+  message: "Too many requests from this IP, please try later",
+});
+
+// security middlware
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(hpp());
+app.use("/api", limiter);
+app.use(cookieParser());
 
 //logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -24,6 +46,24 @@ app.use((err, req, res, next) => {
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
+
+// CORS Configuration
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  }),
+);
 
 // API Routes
 
